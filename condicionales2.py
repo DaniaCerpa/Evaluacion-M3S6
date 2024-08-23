f38b8eb2ab9b4fb61533151684bcefb5acfa02d8
DaniaCerpa <dania.cerpa@gmail.com>
2024-08-23 00:32:58 -0400

    Tercer commit

--- condicionales2.py
+++ condicionales2.py
@@ -1,9 +1,35 @@
-lista_numeros = [1, 15, 7]
+#Los tres numeros "dados" de muestran en una lista
+lista_numeros = [45, 50, 40]
+
+
+#verifico el tipo de dato int para comparar      
+print (type(lista_numeros[0]))
+
+
+if lista_numeros[0]> lista_numeros[1] and lista_numeros[0]> lista_numeros[2]:
+    if lista_numeros[1]> lista_numeros[2]:
+        print(lista_numeros[0], lista_numeros[1], lista_numeros[2])
+    else:
+        print(lista_numeros[0], lista_numeros[2], lista_numeros[1])
+elif lista_numeros[1]> lista_numeros[0] and lista_numeros[1]> lista_numeros[2]:
+    if lista_numeros[0]> lista_numeros[2]:
+        print(lista_numeros[1], lista_numeros[0], lista_numeros[2])
+    else:
+        print(lista_numeros[1], lista_numeros[2], lista_numeros[0])
+elif lista_numeros[2]> lista_numeros[1] and lista_numeros[2]> lista_numeros[0]:
+    if lista_numeros[1]> lista_numeros[0]:
+        print(lista_numeros[2], lista_numeros[1], lista_numeros[0])
+    else:
+        print(lista_numeros[2], lista_numeros[0], lista_numeros[1])        
+
+
+
+"""
+Forma de resolverlo utilizando conocimiento de clases más avanzadas:
 
 def demayoramenor():
     lista_numeros.sort(reverse=True)
     print(lista_numeros)
 
 
-demayoramenor() 
-            
+demayoramenor() """
